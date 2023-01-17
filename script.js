4144500fce3656ce60a1dbeba750baa16ebcf16f
Erik Behrends <behrends@users.noreply.github.com>
2023-01-17 15:10:29 +0100

    Search button mit Event Listener

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Hallo, Wetter!');
+
+function firstFunction() {
+  alert('TODO: Suche ausführen');
+}
+
+// Referenz auf den Button mit der id
+// "search_button" herstellen
+const searchButton = document.getElementById('search_button');
+// Binde Funktion firstFunction an den
+// Button mit der id "search_button"
+searchButton.addEventListener('click', firstFunction);
