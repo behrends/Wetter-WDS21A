dac3d2fad7ec4d934f9d71cbcdd38154e530ffa4
Erik Behrends <behrends@users.noreply.github.com>
2023-01-23 14:37:02 +0100

    Code aufgeräumt

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
+// 1. Schritt: Button "TEST" referenzieren
+const btn = document.getElementById('test');
+const weatherTiles = document.getElementById('weather_tiles');
+
+// 2. Schritt: Verhalten des Buttons definieren:
+// TODO Klick auf Button "TEST" erzeugt Element (Wetterort)
 function createNewLocation() {
-  // 3. Schritt: Neues Element erstellen (<div></div>)
+  // Neues Element erstellen (<div></div>)
   const newElement = document.createElement('div');
-  // 4. Schritt: Inhalt des neuen Elementes setzen
-  // <div>Hamburg</div>
+  // Inhalt des neuen Elementes setzen <div>Hamburg</div>
   newElement.innerHTML = 'Hamburg';
-  // 5. Schritt: Neues Element ins DOM einhängen
-  // a) Element (<div id="weather_tiles">) für die
-  // Wetterorte referenzieren
-  const weatherTiles = document.getElementById('weather_tiles');
-  // b) Neues Element als Kind an weatherTiles einfügen
+  // Neues Element als Kind an weatherTiles einfügen
   weatherTiles.appendChild(newElement);
 }
 
-// TODO: Klick auf Button "TEST" erzeugt Element
-// 1. Schritt: Button "TEST" referenzieren
-const btn = document.getElementById('test');
-// 2. Schritt: Event-Handler (click) anbinden
+// 3. Schritt: Event-Handler (click) anbinden
 // "Funktion an den Button namens Test hängen"
 // () => alert('OK') --> function() {alert('ok')}
 btn.addEventListener('click', createNewLocation);
