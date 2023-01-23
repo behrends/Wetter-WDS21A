359d30b778ca64421bb0c6df93c5c9f8572b18bc
Erik Behrends <behrends@users.noreply.github.com>
2023-01-23 14:17:09 +0100

    Neues Wetterelement hinzufügen (WIP)

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-console.log('Hallo, Wetter!');
-
 function firstFunction() {
   alert('TODO: Suche ausführen');
+}
+
+function createNewLocation() {
+  // 3. Schritt: Neues Element erstellen (<div></div>)
+  const newElement = document.createElement('div');
+  // 4. Schritt: Inhalt des neuen Elementes setzen
+  // <div>Hamburg</div>
+  newElement.innerHTML = 'Hamburg';
+  // 5. Schritt: Neues Element ins DOM einhängen
+  // a) Element (<div id="weather_tiles">) für die
+  // Wetterorte referenzieren
+  const weatherTiles = document.getElementById('weather_tiles');
+  // b) Neues Element als Kind an weatherTiles einfügen
+  weatherTiles.appendChild(newElement);
 }
 
 // Referenz auf den Button mit der id
@@ -10,3 +22,11 @@ const searchButton = document.getElementById('search_button');
 // Binde Funktion firstFunction an den
 // Button mit der id "search_button"
 searchButton.addEventListener('click', firstFunction);
+
+// TODO: Klick auf Button "TEST" erzeugt Element
+// 1. Schritt: Button "TEST" referenzieren
+const btn = document.getElementById('test');
+// 2. Schritt: Event-Handler (click) anbinden
+// "Funktion an den Button namens Test hängen"
+// () => alert('OK') --> function() {alert('ok')}
+btn.addEventListener('click', createNewLocation);
