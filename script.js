a3b7d16b6caff7fa69eef21e6e17e38c71dfb90e
Erik Behrends <behrends@users.noreply.github.com>
2023-01-23 14:46:07 +0100

    HTML durch Template-String

--- script.js
+++ script.js
@@ -2,13 +2,28 @@
 const btn = document.getElementById('test');
 const weatherTiles = document.getElementById('weather_tiles');
 
+// HTML der Wetterorte:
+function getWeatherLocationHTML(location) {
+  return `<div class="weather_location">
+  <div class="place">${location}</div>
+  <div class="weather_info">
+    <span>
+      <span class="temp">7°</span>
+      <span class="cond">zunehmend bewölkt</span>
+    </span>
+    <img src="./sun-cloudy-line.svg" alt="bewölkt" />
+  </div>
+</div>`;
+}
+
 // 2. Schritt: Verhalten des Buttons definieren:
 // TODO Klick auf Button "TEST" erzeugt Element (Wetterort)
 function createNewLocation() {
   // Neues Element erstellen (<div></div>)
   const newElement = document.createElement('div');
   // Inhalt des neuen Elementes setzen <div>Hamburg</div>
-  newElement.innerHTML = 'Hamburg';
+  const location = 'Berlin';
+  newElement.innerHTML = getWeatherLocationHTML(location);
   // Neues Element als Kind an weatherTiles einfügen
   weatherTiles.appendChild(newElement);
 }
